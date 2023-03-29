b5450d9f0fa52ef3ccc852b3c384d02760a0a086
Karam <Karam.arora20@st.niituniversity.in>
2023-03-30 01:07:36 +0530

    backend -edit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express= require('express');
 const app = express();
-const teseract= require('tesseract')
+const tesseract= require('tesseract.js')
 const sharp= require('sharp')
 
 // setting
@@ -9,6 +9,8 @@ let image_link="";
 app.set("port", 4000);
 app.get("/solve/:input",function(req,res){
     image_link= req.params.input
+    res.send(`hello ${image_link}`);
+    console.log("got a solve request")
 })
 console.log(image_link)
 app.use(express.json());
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/:name/:lastname',function(req,res){
 res.send(`<p><b>hello</b> <i>world</i></p><br><p> name: ${req.params.name}<br> last: ${req.params.lastname}</p>`);
 });
+
 app.listen(app.get("port"));
 
 console.log("Server on port", app.get("port"));
--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -2,31 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
-const [value,setvalue]= useState("");
 
-let answer=""
+function App() {
+const [value,setvalue]= useState("");
+let answer="";
+
 async function get_answer(val){
-  let host="localhost:4000/"
+let host="https://localhost:4000/"
 let img_link= host+"solve/"+val
 fetch(img_link).then(result=>result.text()).then(function (data){
   answer=data;
 })
 }
-console.log(answer)
-function App() {
   return (
-    <html>
+  
     <div className="App">
-      <label for="link"> Link</label>
-     <input type="text" name="link" id="link" class="link-box" placeholder="Enter Link to solve the Captcha" 
+      <label htmlFor="link"> Link</label>
+     <input type="text" name="link" id="link" className={"link-box"} placeholder="Enter Link to solve the Captcha" 
      value={value}
      onChange={e => setvalue(e.target.value)} />
-     <button onClick= {get_answer(value)}> Enter </button>
-        <label for="answer"> Link</label>
-     <input type="text" name="answer" id="answer" class="link-box" />
+     <button onClick={() => { get_answer(value);}}> Enter </button>
+        <label htmlFor="answer"> Link</label>
+     <input type="text" name="answer" id="answer" className={"link-box"} />
  
     </div>
-    </html>
+
   );
 }
 
